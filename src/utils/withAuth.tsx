719311eb27c732831ea5035ee49bf9ba0cbea804
DukaSiqueira <e.siqueira@edu.unipar.br>
2024-07-30 12:11:51 -0300

    Teste QR Code

--- src/utils/withAuth.tsx
+++ src/utils/withAuth.tsx
@@ -8,7 +8,7 @@ const withAuth = (WrappedComponent: React.ComponentType) => {
     useEffect(() => {
       const token = localStorage.getItem('token');
       if (!token) {
-        router.push('/login');
+        router.push('/scan-qr');
       }
     }, [router]);
 
