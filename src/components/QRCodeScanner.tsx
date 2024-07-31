a060fae2fef0054028681fe088a31df64bab9810
DukaSiqueira <e.siqueira@edu.unipar.br>
2024-07-31 14:39:34 -0300

    Revert "CHORE: Teste switch cam"
    This reverts commit 9436d05b796c2979af8090bb19a1c49da4baa77a.

--- src/components/QRCodeScanner.tsx
+++ src/components/QRCodeScanner.tsx
@@ -1,6 +1,5 @@
 import dynamic from 'next/dynamic';
 import styled from 'styled-components';
-import { useState, useRef, useEffect } from 'react';
 
 const QrScanner = dynamic(() => import('react-qr-scanner'), { ssr: false });
 
@@ -18,76 +17,11 @@ const Scanner = styled.div`
   margin: auto;
 `;
 
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: space-around;
-  margin-top: 20px;
-`;
-
-const Button = styled.button`
-  padding: 10px 20px;
-  font-size: 16px;
-  cursor: pointer;
-`;
-
 interface QRCodeScannerProps {
   onScan: (data: string | null) => void;
 }
 
 const QRCodeScanner = ({ onScan }: QRCodeScannerProps) => {
-  const [cameraFacing, setCameraFacing] = useState('environment');
-  const [stream, setStream] = useState<MediaStream | null>(null);
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const [videoDevices, setVideoDevices] = useState<MediaDeviceInfo[]>([]);
-  const [currentDeviceIndex, setCurrentDeviceIndex] = useState(0);
-
-  useEffect(() => {
-    const getVideoDevices = async () => {
-      const devices = await navigator.mediaDevices.enumerateDevices();
-      const videoDevices = devices.filter(device => device.kind === 'videoinput');
-      setVideoDevices(videoDevices);
-    };
-
-    getVideoDevices();
-  }, []);
-
-  useEffect(() => {
-    const handleVideo = async (deviceId: string) => {
-      if (stream) {
-        stream.getTracks().forEach((track) => track.stop());
-      }
-
-      const constraints = {
-        video: {
-          deviceId: {
-            exact: deviceId
-          }
-        }
-      };
-
-      try {
-        const newStream = await navigator.mediaDevices.getUserMedia(constraints);
-        setStream(newStream);
-        if (videoRef.current) {
-          videoRef.current.srcObject = newStream;
-          videoRef.current.play();
-        }
-      } catch (error) {
-        console.error('Error accessing camera: ', error);
-      }
-    };
-
-    if (videoDevices.length > 0) {
-      handleVideo(videoDevices[currentDeviceIndex].deviceId);
-    }
-
-    return () => {
-      if (stream) {
-        stream.getTracks().forEach(track => track.stop());
-      }
-    };
-  }, [videoDevices, currentDeviceIndex]);
-
   const handleScan = (data: any) => {
     if (data) {
       onScan(data.text);
@@ -96,10 +30,6 @@ const QRCodeScanner = ({ onScan }: QRCodeScannerProps) => {
 
   const handleError = (error: any) => {
     console.error(error);
-  };
-
-  const handleSwitchCamera = () => {
-    setCurrentDeviceIndex(prevIndex => (prevIndex + 1) % videoDevices.length);
   };
 
   return (
@@ -111,13 +41,7 @@ const QRCodeScanner = ({ onScan }: QRCodeScannerProps) => {
           onScan={handleScan}
           style={{ width: '100%' }}
         />
-        <video ref={videoRef} style={{ display: 'none' }} />
       </Scanner>
-      <ButtonContainer>
-        <Button onClick={handleSwitchCamera}>
-          Alternar Câmera
-        </Button>
-      </ButtonContainer>
     </ScannerContainer>
   );
 };
