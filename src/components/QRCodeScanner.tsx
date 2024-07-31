c16e3119ca3de05ebe94a916c36928f1c02fbb87
DukaSiqueira <e.siqueira@edu.unipar.br>
2024-07-31 11:50:40 -0300

    Revert "CHORE: Teste switch cam"
    This reverts commit ed670eba7904db11654d7d7e172ad3a00a103320.

--- src/components/QRCodeScanner.tsx
+++ src/components/QRCodeScanner.tsx
@@ -1,6 +1,5 @@
 import dynamic from 'next/dynamic';
 import styled from 'styled-components';
-import { useState, useRef, useEffect } from 'react';
 
 const QrScanner = dynamic(() => import('react-qr-scanner'), { ssr: false });
 
@@ -18,58 +17,11 @@ const Scanner = styled.div`
   margin: auto;
 `;
 
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: space-around;
-  margin-top: 20px;
-`;
-
-const Button = styled.button`
-  padding: 10px 20px;
-  font-size: 16px;
-  cursor: pointer;
-`;
-
 interface QRCodeScannerProps {
   onScan: (data: string | null) => void;
 }
 
 const QRCodeScanner = ({ onScan }: QRCodeScannerProps) => {
-  const [cameraFacing, setCameraFacing] = useState('environment');
-  const [stream, setStream] = useState<MediaStream | null>(null);
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  useEffect(() => {
-    const handleVideo = async (cameraFacing: string) => {
-      const constraints = {
-        video: {
-          facingMode: {
-            exact: cameraFacing
-          }
-        }
-      };
-
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        setStream(stream);
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
-          videoRef.current.play();
-        }
-      } catch (error) {
-        console.error('Error accessing camera: ', error);
-      }
-    };
-
-    handleVideo(cameraFacing);
-
-    return () => {
-      if (stream) {
-        stream.getTracks().forEach(track => track.stop());
-      }
-    };
-  }, [cameraFacing]);
-
   const handleScan = (data: any) => {
     if (data) {
       onScan(data.text);
@@ -78,10 +30,6 @@ const QRCodeScanner = ({ onScan }: QRCodeScannerProps) => {
 
   const handleError = (error: any) => {
     console.error(error);
-  };
-
-  const handleSwitchCamera = () => {
-    setCameraFacing(prev => (prev === 'environment' ? 'user' : 'environment'));
   };
 
   return (
@@ -93,13 +41,7 @@ const QRCodeScanner = ({ onScan }: QRCodeScannerProps) => {
           onScan={handleScan}
           style={{ width: '100%' }}
         />
-        <video ref={videoRef} style={{ display: 'none' }} />
       </Scanner>
-      <ButtonContainer>
-        <Button onClick={handleSwitchCamera}>
-          Alternar Câmera
-        </Button>
-      </ButtonContainer>
     </ScannerContainer>
   );
 };
